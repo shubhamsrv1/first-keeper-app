4c00abbe23de169602a61cc7ee39b07fe9818212
shubhamsrv1 <49183670+shubhamsrv1@users.noreply.github.com>
2022-12-01 18:44:53 +0530

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
